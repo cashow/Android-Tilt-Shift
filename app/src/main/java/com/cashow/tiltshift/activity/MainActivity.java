c532ad35cfc55d6f96825abfa88db54bbde83f14
cashow <wy89767@163.com>
2017-03-23 12:35:05 +0800

    引入butterknife，简化activity的代码

--- app/src/main/java/com/cashow/tiltshift/activity/MainActivity.java
+++ app/src/main/java/com/cashow/tiltshift/activity/MainActivity.java
@@ -11,20 +11,28 @@ import com.cashow.tiltshift.R;
 import com.cashow.tiltshift.util.BlurUtil;
 import com.cashow.tiltshift.view.SquareImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * 主界面的activity只需要与blurUtil进行沟通，所有的触摸事件和点击事件都传给blurUtil去处理
  */
 public class MainActivity extends AppCompatActivity {
 
     // 图片控件
-    private SquareImageView photo;
+    @BindView(R.id.photo)
+    SquareImageView photo;
 
     // 隐藏移轴效果的TextView
-    private TextView text_blur_close;
+    @BindView(R.id.text_blur_close)
+    TextView text_blur_close;
     // 切换成径向移轴的TextView
-    private TextView text_blur_round;
+    @BindView(R.id.text_blur_round)
+    TextView text_blur_round;
     // 切换成线性移轴的TextView
-    private TextView text_blur_linear;
+    @BindView(R.id.text_blur_linear)
+    TextView text_blur_linear;
 
     // 处理移轴效果切换的util
     private BlurUtil blurUtil;
@@ -39,18 +47,9 @@ public class MainActivity extends AppCompatActivity {
         initView();
     }
 
-    // 绑定view
-    private void findView() {
-        photo = (SquareImageView) findViewById(R.id.photo);
-        text_blur_close = (TextView) findViewById(R.id.text_blur_close);
-        text_blur_round = (TextView) findViewById(R.id.text_blur_round);
-        text_blur_linear = (TextView) findViewById(R.id.text_blur_linear);
-    }
-
     // 初始化view
     private void initView() {
-        findView();
-        setClickListener();
+        ButterKnife.bind(this);
 
         // 初始化blurUtil
         blurUtil = new BlurUtil(this);
@@ -70,33 +69,21 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
-    // 设置点击事件
-    private void setClickListener() {
-        text_blur_close.setOnClickListener(clickListener);
-        text_blur_linear.setOnClickListener(clickListener);
-        text_blur_round.setOnClickListener(clickListener);
+    @OnClick(R.id.text_blur_close)
+    public void closeBlur(View view) {
+        // 隐藏移轴效果
+        blurUtil.setBlurType(BlurUtil.BLUR_NONE);
     }
 
-    private View.OnClickListener clickListener = new View.OnClickListener() {
+    @OnClick(R.id.text_blur_linear)
+    public void changeToLinear(View view) {
+        // 切换成线性移轴状态
+        blurUtil.setBlurType(BlurUtil.BLUR_LINEAR);
+    }
 
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.text_blur_close:
-                    // 隐藏移轴效果
-                    blurUtil.setBlurType(BlurUtil.BLUR_NONE);
-                    break;
-                case R.id.text_blur_linear:
-                    // 切换成线性移轴状态
-                    blurUtil.setBlurType(BlurUtil.BLUR_LINEAR);
-                    break;
-                case R.id.text_blur_round:
-                    // 切换成径向移轴状态
-                    blurUtil.setBlurType(BlurUtil.BLUR_ROUND);
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
+    @OnClick(R.id.text_blur_round)
+    public void changeToRound(View view) {
+        // 切换成径向移轴状态
+        blurUtil.setBlurType(BlurUtil.BLUR_ROUND);
+    }
 }
